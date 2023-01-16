504366c782db73e7d213a88a08e80440fc858ef8
Anas Besbas <anas.besbas@etu.univ-rouen.fr>
2023-01-16 10:02:09 +0100

    Creating viewHolder

---
+++ app/src/main/java/fr/mastersime/stackoverflow/view/QuestionsViewHolder.kt
@@ -0,0 +1,10 @@
+package fr.mastersime.stackoverflow.view
+
+import androidx.recyclerview.widget.RecyclerView
+import fr.mastersime.stackoverflow.databinding.ViewHolderQuestionsBinding
+
+class QuestionsViewHolder(val binding: ViewHolderQuestionsBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+
+}
