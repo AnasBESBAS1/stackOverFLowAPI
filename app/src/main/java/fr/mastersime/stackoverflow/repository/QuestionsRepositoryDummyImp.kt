482165ddae63d752e16a36e21340ead5302852cf
Anas Besbas <anas.besbas@etu.univ-rouen.fr>
2023-01-16 11:26:39 +0100

    creat dummyImpl for Repo && Inject QuestionRepo with hilt

---
+++ app/src/main/java/fr/mastersime/stackoverflow/repository/QuestionsRepositoryDummyImp.kt
@@ -0,0 +1,19 @@
+package fr.mastersime.stackoverflow.repository
+
+import fr.mastersime.stackoverflow.data.Question
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class QuestionsRepositoryDummyImp @Inject constructor() : QuestionsRepository {
+    override suspend fun getQuestionsList(): List<Question> {
+        delay(2000)
+        return listOf(
+            Question("How can i use css in kotlin", (1..20).random()),
+            Question("How can i use css in kotlin", (1..20).random()),
+            Question("How can i use css in kotlin", (1..20).random()),
+            Question("How can i use css in kotlin", (1..20).random()),
+            Question("How can i use css in kotlin", (1..20).random())
+
+        )
+    }
+}
